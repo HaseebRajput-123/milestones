f404a1a40be47b1459d496d8f89c8df665b7b992
HaseebRajput-123 <haseebrajput059@gmail.com>
2025-01-13 16:15:54 +0500

    Add files via upload

---
+++ milestone-3/script.js
@@ -0,0 +1,35 @@
+"use strict";
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('resume-form');
+    const resumeDisplayElement = document.getElementById('resume-display');
+    if (!form || !resumeDisplayElement) {
+        console.error('Form or Resume Display Element not found in the DOM!');
+        return;
+    }
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const phone = document.getElementById('phone').value;
+        const education = document.getElementById('education').value;
+        const experience = document.getElementById('experiance').value;
+        const skills = document.getElementById('skills').value;
+        const resumeHTML = `
+        <h2><b>Resume</b></h2>
+        <h3>Personal Information</h3>
+        <p><b>Name:</b> ${name}</p>
+        <p><b>Email:</b> ${email}</p>
+        <p><b>Phone:</b> ${phone}</p>
+  
+        <h3>Education</h3>
+        <p>${education}</p>
+  
+        <h3>Experience</h3>
+        <p>${experience}</p>
+  
+        <h3>Skills</h3>
+        <p>${skills}</p>
+      `;
+        resumeDisplayElement.innerHTML = resumeHTML;
+    });
+});
---
+++ milestone-3/script.ts
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('resume-form') as HTMLFormElement;
+    const resumeDisplayElement = document.getElementById('resume-display') as HTMLDivElement;
+  
+    if (!form || !resumeDisplayElement) {
+      console.error('Form or Resume Display Element not found in the DOM!');
+      return;
+    }
+  
+    form.addEventListener('submit', (event: Event) => {
+      event.preventDefault();
+  
+      const name = (document.getElementById('name') as HTMLInputElement).value;
+      const email = (document.getElementById('email') as HTMLInputElement).value;
+      const phone = (document.getElementById('phone') as HTMLInputElement).value;
+      const education = (document.getElementById('education') as HTMLInputElement).value;
+      const experience = (document.getElementById('experiance') as HTMLInputElement).value;
+      const skills = (document.getElementById('skills') as HTMLInputElement).value;
+  
+      const resumeHTML = `
+        <h2><b>Resume</b></h2>
+        <h3>Personal Information</h3>
+        <p><b>Name:</b> ${name}</p>
+        <p><b>Email:</b> ${email}</p>
+        <p><b>Phone:</b> ${phone}</p>
+  
+        <h3>Education</h3>
+        <p>${education}</p>
+  
+        <h3>Experience</h3>
+        <p>${experience}</p>
+  
+        <h3>Skills</h3>
+        <p>${skills}</p>
+      `;
+  
+      resumeDisplayElement.innerHTML = resumeHTML;
+    });
+  });
+  
